54301424ac7f492d6426e392a11bfda29c450efe
Nomolos29 <nomolos2019@gmail.com>
2024-04-11 19:06:34 +0100

    Finished sidebar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import { Navbar } from "./components"
+import { Navbar, Hero } from "./components"
 
-import "./index.scss"
+import "./index.scss" 
 
 
 const App = () => (
     <div>
-        <section><Navbar /></section>
-        <section>Parallax</section>
+        <section id="Homepage"><Navbar /><Hero /></section>
+        <section id="Services">Parallax</section>
         <section>Services</section>
-        <section>Parallax</section>
+        <section id="Portfolio">Parallax</section>
         <section>Portfolio1</section>
         <section>Portfolio2</section>
         <section>Portfolio3</section>
-        <section>Contact</section>
+        <section id="Contact">Contact</section>
     </div>
 )
 
